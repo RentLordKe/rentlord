d40b43325cd97809d7104fd1a5cf75be2e0b701b
maxwellwachira <maxwellwachira67@gmail.com>
2022-09-06 09:17:30 +0300

    removed db migrate script

--- index.ts
+++ index.ts
@@ -1,11 +1,29 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import dotenv from "dotenv";
+
+//Sequelize Database Connector
+import db from "./src/config/dbconfig";
 
 dotenv.config();
 
 const app: Application = express();
+const port = process.env.PORT;
 
-const port = process.env.PORT
+//Database Authentication
+const dbAuthenticate = async () => {
+    try {
+      await db.authenticate();
+      console.log('Connection has been established successfully.');
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+}
+
+dbAuthenticate();
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({ping: "pong"});
+});
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`);

10df6d6e9a1f2d8f35d481390d38fbd1664c1f5f
maxwellwachira <maxwellwachira67@gmail.com>
2022-09-08 15:01:36 +0300

    User Model that Defines DB schema

---
+++ src/users/userModel.ts
@@ -0,0 +1,76 @@
+import { DataTypes, Model, Optional } from 'sequelize';
+
+import db from '../config/dbconfig';
+
+interface UserAttributes {
+    id: number;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    idNumber: number;
+    email: string;
+    password: string;
+    role: "tenant" | "manager" | "owner" | "admin";
+    active: boolean;
+    disabled: boolean;
+}
+
+type UserCreationAttributes = Optional<UserAttributes, 'id'>;
+
+
+export class UserModel extends Model<UserAttributes, UserCreationAttributes> {}
+
+
+UserModel.init({
+   id: {
+    type: DataTypes.BIGINT,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+   },
+   firstName:{
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: "first_name"
+   },
+   lastName:{
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: "last_name"
+   },
+   phoneNumber: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: "phone_number"
+   },
+   idNumber: {
+    type: DataTypes.BIGINT,
+    allowNull: false,
+    field: "id_number"
+   },
+   email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false
+   },
+   password: {
+    type: DataTypes.STRING,
+    allowNull: false
+   },
+   role: {
+    type: DataTypes.STRING,
+    allowNull: false
+   },
+   active: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+   },
+   disabled: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+   }
+},{
+    sequelize: db,
+    tableName: "users",
+    modelName: "User"
+})

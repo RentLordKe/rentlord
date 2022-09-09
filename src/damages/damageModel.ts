b8af91b65de2038ee72862c619045dce59e656ec
maxwellwachira <maxwellwachira67@gmail.com>
2022-09-09 18:02:02 +0300

    Damage Model that Defines DB schema

---
+++ src/damages/damageModel.ts
@@ -0,0 +1,58 @@
+import { DataTypes, Model, Optional } from 'sequelize';
+
+import db from '../config/dbconfig';
+import { PropertyModel } from '../properties/propertyModel';
+import { TenantModel } from '../tenants/tenantModel';
+
+interface DamageAttributes {
+    id: number;
+    description: string;
+    worth: number; 
+    cleared: boolean;
+    TenantId: number;
+    PropertyId: number
+}
+
+type DamageCreationAttributes = Optional<DamageAttributes, 'id'>;
+
+
+export class DamageModel extends Model<DamageAttributes, DamageCreationAttributes> {}
+
+
+DamageModel.init({
+   id: {
+    type: DataTypes.BIGINT,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+   },
+   description:{
+    type: DataTypes.STRING,
+    allowNull: false
+   },
+   worth: {
+    type: DataTypes.STRING,
+    allowNull: false
+   },
+   cleared: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+   },
+   TenantId: {
+    type: DataTypes.BIGINT
+   },
+   PropertyId: {
+    type: DataTypes.BIGINT
+   }
+   
+},{
+    sequelize: db,
+    tableName: "damages",
+    modelName: "Damage",
+});
+
+TenantModel.hasMany(DamageModel);
+DamageModel.belongsTo(TenantModel);
+
+PropertyModel.hasMany(DamageModel);
+DamageModel.belongsTo(PropertyModel);

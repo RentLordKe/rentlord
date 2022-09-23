a086f612848630bc9a20725c8b7dba069e145872
maxwellwachira <maxwellwachira67@gmail.com>
2022-09-23 18:41:06 +0300

    added role on authMiddleware

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -2,26 +2,38 @@ import { Request, Response, NextFunction } from "express";
 import jwt, { JwtPayload, Secret } from "jsonwebtoken";
 import dotenv from "dotenv";
 
+import getUserRole from '../util/getUserRoleUtil';
+
 dotenv.config();
 
-interface UserId extends Request {
-    userId: number;
-}
 
 const SECRET_KEY = process.env.SECRET_KEY as Secret;
 
-export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const token = req.headers.authorization?.split(" ")[1];
-        if (!token) return res.status(401).json({message: "unauthorized"});
+export const authMiddleware = (userRole: 'tenant' | 'manager' | 'owner' | 'admin') => {
 
-        const decodeData = jwt.verify(token, SECRET_KEY) as JwtPayload;
-        (req as UserId).userId = decodeData?.id;
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const token = req.headers.authorization?.split(" ")[1];
+            if (!token) return res.status(401).json({message: "unauthorized"});
 
-        return next();
+            const decodeData = jwt.verify(token, SECRET_KEY) as JwtPayload;
 
-    } catch (error: any) {
-        if (error.name === 'TokenExpiredError') return res.status(401).json({message: "expiredToken"});
-        return res.status(401).json({message: "invalid Token"});
+            //check user role
+            const user = await getUserRole(Number(decodeData?.id));
+            if (!user) return res.status(401).json({message: "unauthorized"});
+            if (user && user.role !== userRole ) return res.status(401).json({message: "unauthorized"});
+
+            res.locals.userId = decodeData?.id;
+
+            return next();
+
+        } catch (error: any) {
+            if (error.name === 'TokenExpiredError') return res.status(401).json({message: "expiredToken"});
+            return res.status(401).json({message: "invalid Token"});
+        }
     }
 }
+
+
+
+
